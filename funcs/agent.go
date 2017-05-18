050cc103b096a36810c09d1417a1b285636d663a
zhangt <zhangt@51idc.com>
2017-05-18 17:06:35 +0800

    修改了agent.alive的命名使其与其他服务监控的存活指标一致

--- funcs/agent.go
+++ funcs/agent.go
@@ -5,5 +5,5 @@ import (
 )
 
 func AgentMetrics() []*model.MetricValue {
-	return []*model.MetricValue{GaugeValue("custom.agent.alive", 1)}
+	return []*model.MetricValue{GaugeValue("Custom.Monitor.alive", 1)}
 }
